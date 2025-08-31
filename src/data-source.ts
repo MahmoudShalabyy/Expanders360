2d0b668046fba615ffc88ed012a324e34056ff3d
MahmoudShalabyy <mahmoudelsayedshalabyy4@gmail.com>
2025-08-31 23:49:26 +0300

    Fix data source 2

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,3 +1,23 @@
+// import { DataSource } from 'typeorm';
+// import { ConfigService } from '@nestjs/config';
+// import * as dotenv from 'dotenv';
+
+// // Load .env file
+// dotenv.config();
+
+// const configService = new ConfigService();
+
+// export const AppDataSource = new DataSource({
+//   type: 'mysql',
+//   host: process.env.MYSQL_HOST,
+//   port: Number(process.env.MYSQL_PORT) || 3306,
+//   username: process.env.MYSQL_USERNAME,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: process.env.MYSQL_DATABASE,
+//   entities: ['src/**/*.entity{.ts,.js}'],
+//   migrations: ['src/migrations/*{.ts,.js}'],
+// });
+
 import { DataSource } from 'typeorm';
 import { ConfigService } from '@nestjs/config';
 import * as dotenv from 'dotenv';
@@ -8,18 +28,12 @@ const configService = new ConfigService();
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: configService.get('MYSQL_HOST'), // استخدام MYSQL_HOST بدل MYSQLHOST
-  port: Number(configService.get('MYSQL_PORT')) || 3306, // Port ديناميكي
-  username: configService.get('MYSQL_USERNAME'), // Username من Railway
-  password: configService.get('MYSQL_PASSWORD'), // Password من Railway
-  database: configService.get('MYSQL_DATABASE') || 'railway', // Database من Railway
+  host: configService.get('MYSQL_HOST'), // بدل MYSQL_HOST
+  port: 41188, // Railway internal mysql port
+  username: 'root', // Railway root user
+  password: configService.get('MYSQL_PASSWORD'), // بدل MYSQL_PASSWORD
+  database: configService.get('MYSQL_DATABASE') || 'railway',
   entities: ['src/**/*.entity{.ts,.js}'],
   migrations: ['src/migrations/*{.ts,.js}'],
-  synchronize: false,
-  extra: {
-    connectionLimit: 3, // عدد الـ Connections المتزامنة
-    connectTimeout: 30000, // 30 ثانية
-    acquireTimeout: 30000, // 30 ثانية
-    queueLimit: 0, // إزالة الحد الأقصى للـ Queue
-  },
 });
+
